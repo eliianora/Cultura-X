16e15bbd8de8502a8a5f0068492203d5a07614b6
Elianora Kouame <167936327+eliianora@users.noreply.github.com>
2025-07-30 13:40:10 +0000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Fonction appelée lorsqu'on clique sur "Discover More"
+function discoverMore() {
+  window.location.href = 'loginPage.html';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const navToggle = document.querySelector('.nav-toggle');
+  const navLinks = document.querySelector('.nav-links');
+  if(navToggle && navLinks) {
+    navToggle.addEventListener('click', function() {
+      navLinks.classList.toggle('show');
+    });
+  }
+});
